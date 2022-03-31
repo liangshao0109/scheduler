519a2f394ff5807451022a42535d0e5f5b6d5fa9
Cynthia <103306002@nccu.edu.tw>
2022-03-31 11:59:51 -0500

    write to firebase

--- src/components/CourseList.js
+++ src/components/CourseList.js
@@ -1,9 +1,18 @@
 import React, { useState } from 'react';
 import Course, { getCourseTerm } from './Course.js';
 
+const scheduleChanged = (selected, courses) => (
+    selected.some(course => course !== courses[course.id])
+);
+
 const CourseList = ({ courses }) => {
     const [term, setTerm] = useState('Fall');
     const [selected, setSelected] = useState([]);
+
+    if (scheduleChanged(selected, courses)) {
+        setSelected([])
+    };
+
     const termCourses = Object.values(courses).filter(course => term === getCourseTerm(course));
 
     return (
